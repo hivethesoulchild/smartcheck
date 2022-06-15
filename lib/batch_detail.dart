3271696d7c1d77719434a88eb245a19fa6e90782
hivethesoulchild <yvancasaxcix@gmail.com>
2022-06-15 18:34:41 +0800

    Added alert dialog

--- lib/batch_detail.dart
+++ lib/batch_detail.dart
@@ -8,8 +8,71 @@ class BatchDetail extends StatelessWidget {
 
   const BatchDetail({Key? key, required this.s_new}) : super(key: key);
 
+
+
   @override
   Widget build(BuildContext context) {
+    showDataAlert() {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              contentPadding: EdgeInsets.only(top: 10.0),
+              title: Text(
+                'Scores',
+                style: GoogleFonts.poppins(),
+              ),
+              content: Container(
+                height: 500,
+                child: SingleChildScrollView(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [Text('English 0')],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [Text('Math 0')],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [Text('Science 0')],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [Text('Aptitude 0')],
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('Status', style: GoogleFonts.poppins(),),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('Recommendations', style: GoogleFonts.poppins(),),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          });
+    }
     return Scaffold(
       backgroundColor: Colors.grey[300],
       appBar: AppBar(
@@ -26,108 +89,48 @@ class BatchDetail extends StatelessWidget {
           child: ListView.builder(
             itemCount: 150,
             itemBuilder: (BuildContext context, int index) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 5.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: Colors.primaries[index & 15],
-                  ),
-                  child: Column(
-                    children: [
-                      Row(
-                        children: const [
-                          Expanded(
-                            child: Text(
-                              'Applicant ID',
-                              softWrap: false,
-                              overflow: TextOverflow.fade,
-                            ),
+              return InkWell(
+                onTap: () {
+                  showDataAlert();
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Colors.primaries[index & 15],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  'Applicant ID',
+                                  softWrap: false,
+                                  overflow: TextOverflow.fade,
+                                  style: GoogleFonts.poppins(fontSize: 14),
+                                ),
+                              ),
+                            ],
                           ),
-                          Expanded(
-                            child: Text(
-                              'English',
-                              softWrap: false,
-                              overflow: TextOverflow.fade,
-                            ),
-                          ),
-                          Expanded(
-                            child: Text(
-                              'Math',
-                              softWrap: false,
-                              overflow: TextOverflow.fade,
-                            ),
-                          ),
-                          Expanded(
-                            child: Text(
-                              'Science',
-                              softWrap: false,
-                              overflow: TextOverflow.fade,
-                            ),
-                          ),
-                          Expanded(
-                            child: Text(
-                              'Aptitude',
-                              softWrap: false,
-                              overflow: TextOverflow.fade,
-                            ),
-                          ),
-                          Expanded(
-                            child: Text(
-                              'Status',
-                              softWrap: false,
-                              overflow: TextOverflow.fade,
-                            ),
-                          ),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  'Name',
+                                  softWrap: false,
+                                  overflow: TextOverflow.fade,
+                                  style: GoogleFonts.poppins(fontSize: 20),
+                                ),
+                              ),
+                            ],
+                          )
                         ],
                       ),
-                      Row(
-                        children: const [
-                          Expanded(
-                            child: Text(
-                              'Name',
-                              softWrap: false,
-                              overflow: TextOverflow.fade,
-                            ),
-                          ),
-                          Expanded(
-                            child: Text(
-                              '0',
-                              softWrap: false,
-                              overflow: TextOverflow.fade,
-                            ),
-                          ),
-                          Expanded(
-                            child: Text(
-                              '0',
-                              softWrap: false,
-                              overflow: TextOverflow.fade,
-                            ),
-                          ),
-                          Expanded(
-                            child: Text(
-                              '0',
-                              softWrap: false,
-                              overflow: TextOverflow.fade,
-                            ),
-                          ),
-                          Expanded(
-                            child: Text(
-                              '0',
-                              softWrap: false,
-                              overflow: TextOverflow.fade,
-                            ),
-                          ),
-                          Expanded(
-                            child: Text(
-                              'Recommendations',
-                              softWrap: false,
-                              overflow: TextOverflow.fade,
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
+                    ),
                   ),
                 ),
               );
