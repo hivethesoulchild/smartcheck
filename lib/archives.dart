6577effc643b3cbe2da876ccd6d31f397c873df5
hivethesoulchild <yvancasaxcix@gmail.com>
2022-06-15 17:38:13 +0800

    Few adjustments

--- lib/archives.dart
+++ lib/archives.dart
@@ -1,14 +1,82 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:hexcolor/hexcolor.dart';
+import 'package:intl/intl.dart';
 
-class Archives extends StatelessWidget {
+class Archives extends StatefulWidget {
   const Archives({Key? key}) : super(key: key);
 
   @override
+  State<Archives> createState() => _ArchivesState();
+}
+
+class _ArchivesState extends State<Archives> {
+  String date = DateFormat("MMMM dd, yyyy").format(DateTime.now());
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Test2'),
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: GridView.builder(
+                  padding: const EdgeInsets.all(10.0),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 1,
+                    mainAxisExtent: 300,
+                  ),
+                  itemBuilder: (BuildContext context, int index) {
+                    return InkWell(
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => Archives()));
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12.0),
+                            color: Colors.grey[500],
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 5,
+                                blurRadius: 7,
+                                offset: Offset(0, 3),
+                              )
+                            ]),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  'Batch $index',
+                                  style: GoogleFonts.poppins(
+                                      fontSize: 22, color: Colors.white),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Align(
+                                alignment: Alignment.topLeft,
+                                child: Text(
+                                  '$date',
+                                  style: GoogleFonts.prompt(
+                                      fontSize: 12, color: Colors.white),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
