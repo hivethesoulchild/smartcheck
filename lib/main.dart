ae085899f6b7a222ef87f4a737c147dd770f96eb
hivethesoulchild <yvancasaxcix@gmail.com>
2022-07-20 13:37:35 +0800

    Welcome page as a main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:smartcheck/login.dart';
+import 'package:smartcheck/welcome.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Login(),
+      home: const WelcomePage(),
     );
   }
 }
