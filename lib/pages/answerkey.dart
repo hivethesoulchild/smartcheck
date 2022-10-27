c94e751f72865b926028d6b061f76d8c2e235890
hivethesoulchild <yvancasaxcix@gmail.com>
2022-10-27 21:22:45 +0800

    fixed page

--- lib/pages/answerkey.dart
+++ lib/pages/answerkey.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:intl/intl.dart';
+import 'package:mongo_dart/mongo_dart.dart';
+import 'package:smartcheck/answer_keys/aptitude_answerkey.dart';
+import 'package:smartcheck/answer_keys/english_answerkey.dart';
+import 'package:smartcheck/answer_keys/math_answerkey.dart';
+import 'package:smartcheck/answer_keys/science_answerkey.dart';
 import 'package:smartcheck/models/create_answerkey.dart';
 
 class AnswerKeyPage extends StatelessWidget {
@@ -10,96 +15,61 @@ class AnswerKeyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String date = DateFormat("MMMM dd, yyyy").format(DateTime.now());
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: HexColor('#35408f')),
-          onPressed: () => Navigator.of(context).pop(),
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          elevation: 0.0,
+          child: new Icon(Icons.save),
+          backgroundColor: HexColor('#35408f'),
+          onPressed: () {},
         ),
-        title: Text(
-          'Answer Key',
-          style: GoogleFonts.poppins(
-            color: HexColor('#35408F'),
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: HexColor('#35408f')),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          title: Text(
+            'Answer Key',
+            style: GoogleFonts.poppins(
+              color: HexColor('#35408F'),
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          bottom: TabBar(
+            physics: BouncingScrollPhysics(),
+            labelColor: HexColor('#35408f'),
+            indicatorColor: HexColor('#35408f'),
+            labelStyle: GoogleFonts.poppins(
+              fontSize: 15,
+            ),
+            unselectedLabelStyle: GoogleFonts.poppins(
+              fontSize: 15,
+            ),
+            tabs: [
+              Tab(
+                text: "English",
+              ),
+              Tab(
+                text: "Science",
+              ),
+              Tab(
+                text: "Math",
+              ),
+              Tab(
+                text: "Aptitude",
+              ),
+            ],
           ),
         ),
-        centerTitle: true,
         backgroundColor: Colors.white,
-      ),
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
+        body: TabBarView(
           children: [
-            Container(
-              alignment: Alignment.topCenter,
-              child: Padding(
-                padding: EdgeInsets.all(20.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => CreateAnswerKey()));
-                  },
-                  style: ElevatedButton.styleFrom(
-                      primary: HexColor('#35408f'),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12.0))),
-                  child: Text(
-                    'Create Answer Key',
-                    style: GoogleFonts.poppins(
-                        fontSize: 16,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w500),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12.0),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 2,
-                      blurRadius: 4,
-                      offset: Offset(0, 3),
-                    )
-                  ],
-                ),
-                child: Column(
-                  children: [
-                    Padding(
-                      padding:
-                          const EdgeInsets.only(left: 16, bottom: 2, top: 10),
-                      child: Align(
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                          'Answer Key 1',
-                          style: GoogleFonts.poppins(
-                              fontSize: 22,
-                              fontWeight: FontWeight.bold,
-                              color: HexColor("#35408f")),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 16, top: 5),
-                      child: Align(
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                          '$date',
-                          style: GoogleFonts.prompt(
-                              fontSize: 12, color: HexColor("#35408f")),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
+            EnglishAnswerKey(),
+            ScienceAnswerKey(),
+            MathAnswerKey(),
+            AptitudeAnserKey(),
           ],
         ),
       ),
