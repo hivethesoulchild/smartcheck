7d1794f3bc6f97819f054bfae5d4574fe8f29c0e
hivethesoulchild <yvancasaxcix@gmail.com>
2022-10-27 21:23:16 +0800

    debug mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:smartcheck/answer_keys/english_answerkey.dart';
 import 'package:smartcheck/models/charts.dart';
+import 'package:smartcheck/pages/answerkey.dart';
 import 'package:smartcheck/pages/dashboard.dart';
 import 'package:smartcheck/pages/login.dart';
 import 'package:smartcheck/pages/scanner.dart';
@@ -7,6 +9,7 @@ import 'package:smartcheck/models/timer.dart';
 import 'package:smartcheck/models/user_dashboard.dart';
 import 'package:smartcheck/models/welcome.dart';
 import 'package:smartcheck/pages/splash_screen.dart';
+import 'package:smartcheck/pages/user_access.dart';
 
 
 void main() {
