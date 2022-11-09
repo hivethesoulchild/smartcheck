9253f11d8abfd087c439bbc918fecc033c6e489e
hivethesoulchild <yvancasaxcix@gmail.com>
2022-11-09 10:28:23 +0800

    added backend and data

--- lib/backend/backendpy.dart
+++ lib/backend/backendpy.dart
@@ -8,23 +8,27 @@ class BackEndPy {
     final response = await http.post(url,
         body: json.encode({'username': 'admin', 'password': 'pass'}));
 
-
     final String responseString = response.body;
     print(responseString);
     return userModelFromJson(responseString);
-
-
   }
+
   static Future checkUser1(String email, String password) async {
     final url = Uri.parse('http://161.97.68.208:9922/checkUser/');
     final response = await http.post(url,
         body: json.encode({'username': 'admin', 'password': 'pass'}));
 
+    final String responseString = response.body;
+    print(responseString);
+    return userModelFromJson(responseString);
+  }
+
+  static Future<dynamic> getAnswerKey() async {
+    final url = Uri.parse('http://161.97.68.208:9922/getAnswerKey/');
+    final response = await http.get(url);
 
     final String responseString = response.body;
-  print(responseString);
-    return userModelFromJson(responseString);
-
-
+    print(responseString);
+    return responseString;
   }
 }
--- lib/data.dart
+++ lib/data.dart
@@ -1,1 +1,2 @@
 var batchData = [];
+var answerkey = [];
