ab2379e7507a7d7daff6a077a2f25cd83e6d8ab0
hivethesoulchild <yvancasaxcix@gmail.com>
2023-06-04 20:10:14 +0800

    added clean-up code

--- lib/models/create_answerkey.dart
+++ lib/models/create_answerkey.dart
@@ -292,3 +292,154 @@ class _CreateAnswerKeyState extends State<CreateAnswerKey> {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+// import 'package:hexcolor/hexcolor.dart';
+
+// class CreateAnswerKey extends StatefulWidget {
+//   const CreateAnswerKey({Key? key}) : super(key: key);
+
+//   @override
+//   State<CreateAnswerKey> createState() => _CreateAnswerKeyState();
+// }
+
+// class _CreateAnswerKeyState extends State<CreateAnswerKey> {
+//   List<bool> _answerPressed = [false, false, false, false];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         leading: IconButton(
+//           icon: Icon(Icons.arrow_back, color: HexColor('#35408f')),
+//           onPressed: () => Navigator.of(context).pop(),
+//         ),
+//         title: Text(
+//           'Answer Key',
+//           style: GoogleFonts.poppins(
+//             color: HexColor('#35408F'),
+//           ),
+//         ),
+//         centerTitle: true,
+//         backgroundColor: Colors.white,
+//       ),
+//       body: Center(
+//         child: Column(
+//           children: [
+//             Container(
+//               alignment: Alignment.topCenter,
+//               child: Padding(
+//                 padding: const EdgeInsets.all(20.0),
+//                 child: ElevatedButton(
+//                   style: ElevatedButton.styleFrom(
+//                     backgroundColor: HexColor('#35408f'),
+//                     shape: RoundedRectangleBorder(
+//                       borderRadius: BorderRadius.circular(12.0),
+//                     ),
+//                   ),
+//                   onPressed: () {},
+//                   child: Text(
+//                     'Upload Answer Key',
+//                     style: GoogleFonts.poppins(
+//                       fontSize: 16,
+//                       color: Colors.white,
+//                       fontWeight: FontWeight.w500,
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             buildAnswerRow(1),
+//             SizedBox(height: 10),
+//             buildAnswerRow(2),
+//             SizedBox(height: 10),
+//             buildAnswerRow(3),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+//   Row buildAnswerRow(int questionNumber) {
+//     return Row(
+//       children: <Widget>[
+//         Padding(
+//           padding: const EdgeInsets.only(left: 5),
+//           child: Text(
+//             '$questionNumber. ',
+//             style: GoogleFonts.nunito(color: Colors.black),
+//           ),
+//         ),
+//         ElevatedButton(
+//           onPressed: () {
+//             setState(() {
+//               _answerPressed[0] = !_answerPressed[0];
+//             });
+//           },
+//           child: Text(
+//             'A',
+//             style: GoogleFonts.nunito(color: Colors.black),
+//           ),
+//           style: ElevatedButton.styleFrom(
+//             shape: CircleBorder(),
+//             backgroundColor:
+//                 _answerPressed[0] ? Colors.green[100] : Colors.white,
+//             padding: EdgeInsets.all(20),
+//           ),
+//         ),
+//         ElevatedButton(
+//           onPressed: () {
+//             setState(() {
+//               _answerPressed[1] = !_answerPressed[1];
+//             });
+//           },
+//           child: Text(
+//             'B',
+//             style: GoogleFonts.nunito(color: Colors.black),
+//           ),
+//           style: ElevatedButton.styleFrom(
+//             shape: CircleBorder(),
+//             backgroundColor:
+//                 _answerPressed[1] ? Colors.green[100] : Colors.white,
+//             padding: EdgeInsets.all(20),
+//           ),
+//         ),
+//         ElevatedButton(
+//           onPressed: () {
+//             setState(() {
+//               _answerPressed[2] = !_answerPressed[2];
+//             });
+//           },
+//           child: Text(
+//             'C',
+//             style: GoogleFonts.nunito(color: Colors.black),
+//           ),
+//           style: ElevatedButton.styleFrom(
+//             shape: CircleBorder(),
+//             backgroundColor:
+//                 _answerPressed[2] ? Colors.green[100] : Colors.white,
+//             padding: EdgeInsets.all(20),
+//           ),
+//         ),
+//         ElevatedButton(
+//           onPressed: () {
+//             setState(() {
+//               _answerPressed[3] = !_answerPressed[3];
+//             });
+//           },
+//           child: Text(
+//             'D',
+//             style: GoogleFonts.nunito(color: Colors.black),
+//           ),
+//           style: ElevatedButton.styleFrom(
+//             shape: CircleBorder(),
+//             backgroundColor:
+//                 _answerPressed[3] ? Colors.green[100] : Colors.white,
+//             padding: EdgeInsets.all(20),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
