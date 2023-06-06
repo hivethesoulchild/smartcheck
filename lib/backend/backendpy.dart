4b042ca55f0b663ca8dd2da45da338d789760e71
nona? <79404245+not-nonano@users.noreply.github.com>
2023-06-06 09:14:28 +0800

    added api on AddUser

--- lib/backend/backendpy.dart
+++ lib/backend/backendpy.dart
@@ -1,136 +1,118 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:smartcheck/apiModel//usermodel.dart';
 
 class BackEndPy {
-  static Future<dynamic> checkUser(String email, String password) async {
-    final url = Uri.parse('http://161.97.68.208:9922/checkUser/');
-    final response = await http.post(url,
-        body: json.encode({'username': email, 'password': password}));
+  static const String baseUrl = 'http://154.26.132.250:9090/';
 
-    var responseString = jsonDecode(response.body) as Map<String, dynamic>;
-    return responseString;
+  static Future<dynamic> checkUser(String email, String password) async {
+    final url = Uri.parse('$baseUrl/checkUser/');
+    final response = await http.post(url, body: json.encode({'username': email, 'password': password}));
+    return jsonDecode(response.body);
   }
 
   static Future<dynamic> getAnswerKey() async {
-    final url = Uri.parse('http://161.97.68.208:9922/getAnswerKey/');
+    final url = Uri.parse('$baseUrl/getAnswerKey/');
     final response = await http.get(url);
-
-    var jsonResponse = jsonDecode(response.body) as Map<String, dynamic>;
-    return jsonResponse;
+    return jsonDecode(response.body);
   }
 
   static void updateAnswerKey(String id, List data) async {
-    final url = Uri.parse('http://161.97.68.208:9922/updateAnswerKey/');
-    final response = await http.patch(url,
-        body: json.encode({
-          'id': '1',
-          'english': data[0],
-          'science': data[1],
-          'mathematics': data[2],
-          'aptitude': data[3]
-        }));
+    final url = Uri.parse('$baseUrl/updateAnswerKey/');
+    final response = await http.patch(url, body: json.encode({
+      'id': '1',
+      'english': data[0],
+      'science': data[1],
+      'mathematics': data[2],
+      'aptitude': data[3]
+    }));
 
     print(response.body);
   }
 
   static Future<dynamic> getAllUser() async {
-    final url = Uri.parse('http://161.97.68.208:9922/getAllUser/');
+    final url = Uri.parse('$baseUrl/getAllUser/');
     final response = await http.get(url);
-
-    var jsonResponse = jsonDecode(response.body) as List<dynamic>;
-
-    return jsonResponse;
+    return jsonDecode(response.body) as List<dynamic>;
   }
 
-  static void addApplicantList(String id, String schoolName, List applicants,
-      String username, String date, bool archive) async {
-    final url = Uri.parse('http://161.97.68.208:9922/addApplicantList/');
-    final response = await http.post(url,
-        body: json.encode({
-          '_id': id,
-          'schoolName': schoolName,
-          'applicants': applicants,
-          'proctor': username,
-          'date': date,
-          'archive': archive
-        }));
+  static void addApplicantList(String id, String schoolName, List applicants, String username, String date, bool archive) async {
+    final url = Uri.parse('$baseUrl/addApplicantList/');
+    final response = await http.post(url, body: json.encode({
+      '_id': id,
+      'schoolName': schoolName,
+      'applicants': applicants,
+      'proctor': username,
+      'date': date,
+      'archive': archive
+    }));
 
     var jsonResponse = jsonDecode(response.body) as Map<String, dynamic>;
-
     print(jsonResponse);
   }
 
   static Future<dynamic> getAllApplicantList() async {
-    final url = Uri.parse('http://161.97.68.208:9922/getAllApplicantList/');
+    final url = Uri.parse('$baseUrl/getAllApplicantList/');
     final response = await http.get(url);
-
-    var responseString = jsonDecode(response.body) as List<dynamic>;
-    return responseString;
+    return jsonDecode(response.body) as List<dynamic>;
   }
 
-  static void getApplicantList(
-      String id, String schoolName, List applicants) async {
-    final url = Uri.parse('http://161.97.68.208:9922/addApplicantList/');
-    final response = await http.post(url,
-        body: json.encode(
-            {'_id': id, 'schoolName': schoolName, 'applicants': applicants}));
+  static void getApplicantList(String id, String schoolName, List applicants) async {
+    final url = Uri.parse('$baseUrl/addApplicantList/');
+    final response = await http.post(url, body: json.encode({
+      '_id': id,
+      'schoolName': schoolName,
+      'applicants': applicants
+    }));
 
     var jsonResponse = jsonDecode(response.body) as Map<String, dynamic>;
-
     print(jsonResponse);
   }
 
   static void editApplicantList(String id, bool archive) async {
-    final url = Uri.parse('http://161.97.68.208:9922/editApplicantList/');
-    final response = await http.patch(url,
-        body: json.encode({'_id': id, 'archive': archive}));
+    final url = Uri.parse('$baseUrl/editApplicantList/');
+    final response = await http.patch(url, body: json.encode({
+      '_id': id,
+      'archive': archive
+    }));
 
     var jsonResponse = jsonDecode(response.body) as Map<String, dynamic>;
-
     print(jsonResponse);
   }
 
   static void deleteApplicantList(String id) async {
-    final url = Uri.parse('http://161.97.68.208:9922/deleteApplicantList/');
+    final url = Uri.parse('$baseUrl/deleteApplicantList/');
     final response = await http.delete(url, body: json.encode({'_id': id}));
 
     var jsonResponse = jsonDecode(response.body) as Map<String, dynamic>;
-
     print(jsonResponse);
   }
 
-  static void addUser(String id, String username, String password,
-      bool isActive, String role) async {
-    final url = Uri.parse('http://161.97.68.208:9922/addUser/');
-    final response = await http.post(url,
-        body: json.encode({
-          '_id': id,
-          'username': username,
-          'password': password,
-          'isActive': isActive,
-          'role': role
-        }));
+  static void addUser(String username, String password, bool isActive, String role) async {
+    final url = Uri.parse('$baseUrl/addUser/');
+    final response = await http.post(url, body: json.encode({
+      'username': username,
+      'password': password,
+      'isActive': isActive,
+      'role': role
+    }));
 
     print(response.body);
   }
 
-  static void editUser(
-      String id, String password, bool isActive, String role) async {
-    final url = Uri.parse('http://161.97.68.208:9922/editUser/');
-    final response = await http.patch(url,
-        body: json.encode({
-          '_id': id,
-          'password': password,
-          'isActive': isActive,
-          'role': role
-        }));
+  static void editUser(String id, String password, bool isActive, String role) async {
+    final url = Uri.parse('$baseUrl/editUser/');
+    final response = await http.patch(url, body: json.encode({
+      '_id': id,
+      'password': password,
+      'isActive': isActive,
+      'role': role
+    }));
 
     print(response.body);
   }
 
   static void deleteUser(String id) async {
-    final url = Uri.parse('http://161.97.68.208:9922/deleteUser/');
+    final url = Uri.parse('$baseUrl/deleteUser/');
     final response = await http.delete(url, body: json.encode({'_id': id}));
 
     print(response.body);
--- lib/routes/adduserdialog.dart
+++ lib/routes/adduserdialog.dart
@@ -1,77 +1,168 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:smartcheck/backend/backendpy.dart';
 
-class AddUserDialog extends StatelessWidget {
+class AddUserDialog extends StatefulWidget {
   const AddUserDialog({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    TextEditingController _textFieldController = TextEditingController();
-    showAddUserDialog(BuildContext context) {
-      AlertDialog alert = AlertDialog(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20.0),
-        ),
-        contentPadding: EdgeInsets.only(top: 10.0),
-        title: Text(
-          'Add User',
-          style: GoogleFonts.poppins(),
-        ),
-        content: SingleChildScrollView(
-          padding: EdgeInsets.all(8),
-          child: Column(
-            children: [
-              TextFormField(
-                controller: _textFieldController,
-                decoration: InputDecoration(
-                  hintText: 'Username',
-                ),
-              ),
-              TextFormField(
-                controller: _textFieldController,
-                decoration: InputDecoration(
-                  hintText: 'Password',
-                ),
-              ),
-            ],
-          ),
-        ),
-        actions: [
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text('Done'),
-          ),
-        ],
-      );
-      showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return alert;
-          });
-    }
+  _AddUserDialogState createState() => _AddUserDialogState();
+}
 
+class _AddUserDialogState extends State<AddUserDialog> {
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  void showAddUserDialog(BuildContext context, String username, String password) {
+    AlertDialog alert = AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      contentPadding: EdgeInsets.only(top: 10.0),
+      title: Text(
+        'Add User',
+        style: GoogleFonts.poppins(),
+      ),
+      content: SingleChildScrollView(
+        padding: EdgeInsets.all(8),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                hintText: 'Username',
+              ),
+            ),
+            TextFormField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                hintText: 'Password',
+              ),
+            ),
+          ],
+        ),
+      ),
+      actions: [
+        ElevatedButton(
+          onPressed: () {
+            print('Username: $username');
+            print('Password: $password');
+
+            BackEndPy.addUser(username, password, true, 'admin');
+
+            Navigator.pop(context);
+          },
+          child: Text('Done'),
+        ),
+      ],
+    );
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.topCenter,
       child: Padding(
         padding: EdgeInsets.all(20.0),
         child: ElevatedButton(
           onPressed: () {
-            showAddUserDialog(context);
+            showAddUserDialog(context, _usernameController.text, _passwordController.text);
           },
           style: ElevatedButton.styleFrom(
-              primary: HexColor('#35408f'),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12.0))),
+            primary: HexColor('#35408f'),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+          ),
           child: Text(
             'Add User',
-            style: GoogleFonts.poppins(
-                fontSize: 16, color: Colors.white, fontWeight: FontWeight.w500),
+            style: GoogleFonts.poppins(fontSize: 16, color: Colors.white, fontWeight: FontWeight.w500),
           ),
         ),
       ),
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+// import 'package:hexcolor/hexcolor.dart';
+
+// class AddUserDialog extends StatelessWidget {
+//   const AddUserDialog({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     TextEditingController _textFieldController = TextEditingController();
+//     showAddUserDialog(BuildContext context) {
+//       AlertDialog alert = AlertDialog(
+//         shape: RoundedRectangleBorder(
+//           borderRadius: BorderRadius.circular(20.0),
+//         ),
+//         contentPadding: EdgeInsets.only(top: 10.0),
+//         title: Text(
+//           'Add User',
+//           style: GoogleFonts.poppins(),
+//         ),
+//         content: SingleChildScrollView(
+//           padding: EdgeInsets.all(8),
+//           child: Column(
+//             children: [
+//               TextFormField(
+//                 controller: _textFieldController,
+//                 decoration: InputDecoration(
+//                   hintText: 'Username',
+//                 ),
+//               ),
+//               TextFormField(
+//                 controller: _textFieldController,
+//                 decoration: InputDecoration(
+//                   hintText: 'Password',
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//         actions: [
+//           ElevatedButton(
+//             onPressed: () {
+//               Navigator.pop(context);
+//             },
+//             child: Text('Done'),
+//           ),
+//         ],
+//       );
+//       showDialog(
+//           context: context,
+//           builder: (BuildContext context) {
+//             return alert;
+//           });
+//     }
+
+//     return Container(
+//       alignment: Alignment.topCenter,
+//       child: Padding(
+//         padding: EdgeInsets.all(20.0),
+//         child: ElevatedButton(
+//           onPressed: () {
+//             showAddUserDialog(context);
+//           },
+//           style: ElevatedButton.styleFrom(
+//               primary: HexColor('#35408f'),
+//               shape: RoundedRectangleBorder(
+//                   borderRadius: BorderRadius.circular(12.0))),
+//           child: Text(
+//             'Add User',
+//             style: GoogleFonts.poppins(
+//                 fontSize: 16, color: Colors.white, fontWeight: FontWeight.w500),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
