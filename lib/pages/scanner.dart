36a1c807fc10c67527e0f5551ef1a27e42c17dcc
hivethesoulchild <yvancasaxcix@gmail.com>
2023-06-29 01:35:06 +0800

    fixxxx

--- lib/pages/scanner.dart
+++ lib/pages/scanner.dart
@@ -11,9 +11,10 @@ import 'package:smartcheck/backend/backendpy.dart';
 
 class ScannerPage extends StatefulWidget {
   final List<CameraDescription> cameras;
-  final dynamic id; 
+  final dynamic id;
 
-  const ScannerPage({Key? key, required this.cameras, required this.id}) : super(key: key);
+  const ScannerPage({Key? key, required this.cameras, required this.id})
+      : super(key: key);
 
   @override
   State<ScannerPage> createState() => _ScannerPageState();
@@ -249,8 +250,8 @@ class _ScannerPageState extends State<ScannerPage> {
                           child: Center(
                             child: SizedBox(
                               height: MediaQuery.of(context).size.width < 360
-                                  ? 600
-                                  : 700,
+                                  ? 550
+                                  : 630,
                               width: double.infinity,
                               child: CameraPreview(cameraController),
                             ),
