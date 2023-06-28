472bb3eab18514b46d5ba047266e26fe38c361cd
hivethesoulchild <yvancasaxcix@gmail.com>
2023-06-28 16:51:23 +0800

    dynamic

--- lib/answer_keys/aptitude_answerkey.dart
+++ lib/answer_keys/aptitude_answerkey.dart
@@ -20,141 +20,26 @@ class _AptitudeAnserKeyState extends State<AptitudeAnserKey> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("1. "),
-                  ),
-                  Center(child: SizedBox(height: 50, child: InputChipotle(answer: global.answer_key[3][0], number: "1", subject: 'aptitude',),width: 300,)),
-                ],
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("2. "),
-                  ),
-                  Center(child: SizedBox(height: 50, child: InputChipotle(answer: global.answer_key[3][1], number: "2",subject: 'aptitude',),width: 300,)),
-                ],
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("3. "),
-                  ),
-                  Center(child: SizedBox(height: 50, child: InputChipotle(answer: global.answer_key[3][2], number: "3",subject: 'aptitude',),width: 300,)),
-                ],
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("4. "),
-                  ),
-                  Center(child: SizedBox(height: 50, child: InputChipotle(answer: global.answer_key[3][3], number: "4",subject: 'aptitude',),width: 300,)),
-                ],
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("5. "),
-                  ),
-                  Center(child: SizedBox(height: 50, child: InputChipotle(answer: global.answer_key[3][4], number: "5",subject: 'aptitude',),width: 300,)),
-                ],
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("6. "),
-                  ),
-                  Center(child: SizedBox(height: 50, child: InputChipotle(answer: global.answer_key[3][5], number: "6",subject: 'aptitude',),width: 300,)),
-                ],
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("7. "),
-                  ),
-                  Center(child: SizedBox(height: 50, child: InputChipotle(answer: global.answer_key[3][6], number: "7",subject: 'aptitude',),width: 300,)),
-                ],
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("8. "),
-                  ),
-                  Center(child: SizedBox(height: 50, child: InputChipotle(answer: global.answer_key[3][7], number: "8",subject: 'aptitude',),width: 300,)),
-                ],
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("9. "),
-                  ),
-                  Center(child: SizedBox(height: 50, child: InputChipotle(answer: global.answer_key[3][8], number: "9",subject: 'aptitude',),width: 300,)),
-                ],
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("10. "),
-                  ),
-                  Center(child: SizedBox(height: 50, child: InputChipotle(answer: global.answer_key[3][9], number: "10",subject: 'aptitude',),width: 300,)),
-                ],
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("11. "),
-                  ),
-                  Center(child: SizedBox(height: 50, child: InputChipotle(answer: global.answer_key[3][10], number: "11",subject: 'aptitude',),width: 300,)),
-                ],
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("12. "),
-                  ),
-                  Center(child: SizedBox(height: 50, child: InputChipotle(answer: global.answer_key[3][11], number: "12",subject: 'aptitude',),width: 300,)),
-                ],
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("13. "),
-                  ),
-                  Center(child: SizedBox(height: 50, child: InputChipotle(answer: global.answer_key[3][12], number: "13",subject: 'aptitude',),width: 300,)),
-                ],
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("14. "),
-                  ),
-                  Center(child: SizedBox(height: 50, child: InputChipotle(answer: global.answer_key[3][13], number: "14",subject: 'aptitude',),width: 300,)),
-                ],
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("15. "),
-                  ),
-                  Center(child: SizedBox(height: 50, child: InputChipotle(answer: global.answer_key[3][14], number: "15",subject: 'aptitude',),width: 300,)),
-                ],
-              ),
+              for (int i = 0; i < global.answer_key[3].length; i++)
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("${i + 1}. "),
+                    ),
+                    Center(
+                      child: SizedBox(
+                        height: 50,
+                        child: InputChipotle(
+                          answer: global.answer_key[3][i],
+                          number: "${i + 1}",
+                          subject: 'aptitude',
+                        ),
+                        width: 300,
+                      ),
+                    ),
+                  ],
+                ),
             ],
           ),
         ),
@@ -162,6 +47,7 @@ class _AptitudeAnserKeyState extends State<AptitudeAnserKey> {
     );
   }
 }
+
 
 // TODO: API Integration Pareho na rin sa the rest of the subjects if prefer mo na ganito
 
