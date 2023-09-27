620a9ce53d08b6b720fcd72beb9901a0033be820
hivethesoulchild <yvancasaxcix@gmail.com>
2023-09-28 06:56:13 +0800

    fix problems within gradle kotlin

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,8 @@ import Foundation
 
 import file_support
 import path_provider_foundation
-import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSupportPlugin.register(with: registry.registrar(forPlugin: "FileSupportPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
