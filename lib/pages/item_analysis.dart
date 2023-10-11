6f5a0f906725b1a2865dbf00b4a4a79defe38d23
hivethesoulchild <yvancasaxcix@gmail.com>
2023-10-11 18:30:21 +0800

    add path provider

--- lib/pages/item_analysis.dart
+++ lib/pages/item_analysis.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
@@ -6,6 +8,30 @@ import 'package:smartcheck/charts/english_analysis.dart';
 import 'package:smartcheck/charts/math_analysis.dart';
 import 'package:smartcheck/charts/science_analysis.dart';
 import 'package:smartcheck/routes/analysis_per_subject_button.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+Future<void> exportData() async {
+  // Get the document directory
+  final directory = await getApplicationDocumentsDirectory();
+
+  // Define the file path where you want to save your exported data
+  final filePath = '${directory.path}/exported_data.txt';
+
+  // Implement your export logic here
+  // For example, you can write data to the file using the File class:
+  final file = File(filePath);
+  await file.writeAsString('Your exported data goes here.');
+
+  // Show a toast message to confirm the export
+  Fluttertoast.showToast(
+    msg: 'Data exported successfully!',
+    toastLength: Toast.LENGTH_SHORT, // You can customize the duration
+    gravity: ToastGravity.BOTTOM, // You can customize the position
+    backgroundColor: Colors.green, // You can customize the background color
+    textColor: Colors.white, // You can customize the text color
+  );
+}
 
 class ItemAnalysis extends StatefulWidget {
   const ItemAnalysis({Key? key}) : super(key: key);
@@ -15,6 +41,9 @@ class ItemAnalysis extends StatefulWidget {
 }
 
 class _ItemAnalysisState extends State<ItemAnalysis> {
+  void exportData() {
+    // add logic here
+  }
   @override
   Widget build(BuildContext context) => DefaultTabController(
         length: 4,
@@ -43,7 +72,7 @@ class _ItemAnalysisState extends State<ItemAnalysis> {
                   ),
                   itemBuilder: (context) => [
                     PopupMenuItem(
-                      onTap: (() {}),
+                      onTap: exportData,
                       value: 1,
                       child: const Text('Export'),
                     )
