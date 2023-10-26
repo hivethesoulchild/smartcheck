346cb3a7d8687c157a0490f5cd3bee1546cdaf51
nona? <79404245+not-nonano@users.noreply.github.com>
2023-10-26 11:34:15 +0800

    Update english_analysis.dart

--- lib/charts/english_analysis.dart
+++ lib/charts/english_analysis.dart
@@ -29,6 +29,7 @@ class EnglishAnalysis extends StatefulWidget {
 }
 
 class _EnglishAnalysisState extends State<EnglishAnalysis> {
+  final api = await BackEndPy.getAnalysisDataEnglish();
   final List<Item> _data = List<Item>.generate(
     30,
     (int index) => Item(
@@ -41,13 +42,13 @@ class _EnglishAnalysisState extends State<EnglishAnalysis> {
           data: [
             {
               'label': 'A',
-              'value': 10,
-              'color': Colors.green
+              'value':  api['aptitudeCount']['A'][index],
+              'color': Colors.blue
             }, // Correct answer
-            {'label': 'B', 'value': 20, 'color': Colors.red},
-            {'label': 'C', 'value': 30, 'color': Colors.red},
-            {'label': 'D', 'value': 15, 'color': Colors.red},
-            {'label': 'E', 'value': 24, 'color': Colors.red},
+            {'label': 'B', 'value': api['aptitudeCount']['B'][index], 'color': Colors.blue},
+            {'label': 'C', 'value': api['aptitudeCount']['C'][index], 'color': Colors.blue},
+            {'label': 'D', 'value': api['aptitudeCount']['D'][index], 'color': Colors.blue},
+            {'label': 'E', 'value': api['aptitudeCount']['E'][index], 'color': Colors.blue},
           ],
           colorFn: (dynamic data, _) =>
               charts.ColorUtil.fromDartColor(data['color']),
