72dd5836ce3ce94038cc3502cc142d8a01273103
hivethesoulchild <peytonsoulchild@gmail.com>
2024-03-04 01:22:34 +0800

    initial commit

---
+++ lib/models/usermanual.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
+class UserManual extends StatefulWidget {
+  const UserManual({super.key});
+
+  @override
+  State<UserManual> createState() => _UserManualState();
+}
+
+class _UserManualState extends State<UserManual> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: HexColor('#35408f')),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        centerTitle: true,
+        title: Text(
+          "User Manual",
+          style: GoogleFonts.nunito(
+            color: HexColor('#35408F'),
+          ),
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: SfPdfViewer.asset(""),
+    );
+  }
+}
